9e38207ad1ccfc6544576f8b66a96885fb0c22d7
sai-ram-sagar <sairamsagar0912@gmail.com>
2025-03-20 12:05:46 +0530

    Updated backend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
